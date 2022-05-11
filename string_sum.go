6f3841716c47134d691c31b2333827528ba284c2
Pavel Gordiyanov <pgordiyanov@slb.com>
2022-05-11 15:54:50 +0600

    add placeholder and description

---
+++ string_sum.go
@@ -0,0 +1,26 @@
+package string_sum
+
+import (
+	"errors"
+)
+
+//use these errors as appropriate, wrapping them with fmt.Errorf function
+var (
+	// Use when the input is empty, and input is considered empty if the string contains only whitespace
+	errorEmptyInput     = errors.New("input is empty")
+	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+)
+
+// Implement a function that computes the sum of two int numbers written as a string
+// For example, having an input string "3+5", it should return output string "8" and nil error
+// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
+//
+//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
+// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
+// with fmt.Errorf function
+//
+// Use the errors defined above as described, again wrapping into fmt.Errorf
+
+func StringSum(input string) (output string, err error) {
+	return "", nil
+}
