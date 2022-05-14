7d63cecd705d2fad0b6af1dc261958bdbd9567dd
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-05-14 13:03:25 +0200

    modified:   string_sum.go

--- string_sum.go
+++ string_sum.go
@@ -3,113 +3,107 @@ package string_sum
 import (
 	"errors"
 	"fmt"
-	"reflect"
+	"regexp"
 	"strconv"
-	"testing"
+	"strings"
 )
 
-func TestFailWhenEmptyInputStringSum(t *testing.T) {
-	s := "   "
-	expected := ""
+//use these errors as appropriate, wrapping them with fmt.Errorf function
+var (
+	// Use when the input is empty, and input is considered empty if the string contains only whitespace
+	errorEmptyInput = errors.New("input is empty")
+	// Use when the expression has number of operands not equal to two
+	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+)
 
-	actual, err := StringSum(s)
+// Implement a function that computes the sum of two int numbers written as a string
+// For example, having an input string "3+5", it should return output string "8" and nil error
+// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
+//
+//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
+// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
+// with fmt.Errorf function
+//
+// Use the errors defined above as described, again wrapping into fmt.Errorf
 
-	if !reflect.DeepEqual(actual, expected) || err == nil || err.Error() != "e1: input is empty" {
-		fmt.Println(expected, len(expected))
-		fmt.Println(actual, len(actual))
-		t.Errorf("error, man")
-	}
-}
-
-func TestFailWhenLessTwoOperands(t *testing.T) {
-	s := "555"
-	expected := ""
-
-	actual, err := StringSum(s)
-
-	errorMessage := "e2: expecting two operands, but received more or less"
-	if !reflect.DeepEqual(actual, expected) || err == nil || errors.Unwrap(err).Error() == errorMessage {
-		fmt.Println(expected, len(expected))
-		fmt.Println(actual, len(actual))
-		t.Errorf(err.Error())
-	}
-}
-
-func TestFailWhenTooMuchOperands(t *testing.T) {
-	s := "-1 - 2 + 88"
-	expected := ""
-
-	actual, err := StringSum(s)
-
-	errorMessage := "e3: expecting two operands, but received more or less"
-	if !reflect.DeepEqual(actual, expected) || err == nil || errors.Unwrap(err).Error() == errorMessage {
-		fmt.Println(expected, len(expected))
-		fmt.Println(actual, len(actual))
-		t.Errorf(err.Error())
-	}
-}
-
-func TestFailWhenInputIncorrect(t *testing.T) {
-	s := "24c+55"
-	expected := ""
-	expectedErr := &strconv.NumError{Func: "Atoi", Num: "24c", Err: strconv.ErrSyntax}
-
-	actual, err := StringSum(s)
-
-	if !reflect.DeepEqual(actual, expected) || err == nil || !reflect.DeepEqual(errors.Unwrap(err), expectedErr) {
-		fmt.Println(expected, len(expected))
-		fmt.Println(actual, len(actual))
-		t.Errorf(err.Error())
-	}
-}
-
-func TestSumOfPositiveOperands(t *testing.T) {
-	s := "11 + 33"
-	expected := "44"
-
-	actual, err := StringSum(s)
-
-	if err != nil {
-		t.Errorf(err.Error())
+func StringSum(input string) (output string, err error) {
+	input = strings.TrimSpace(input)
+	if len(input) == 0 {
+		return "", fmt.Errorf("e1: %w", errorEmptyInput)
 	}
 
-	if !reflect.DeepEqual(expected, actual) {
-		fmt.Println(expected)
-		fmt.Println(actual)
-		t.Errorf("error, man")
-	}
-}
-
-func TestSumOfDifferentOperands(t *testing.T) {
-	s := "-50 + 1"
-	expected := "-49"
-
-	actual, err := StringSum(s)
-
-	if err != nil {
-		t.Errorf(err.Error())
+	lessPatern := `^[\s\+-]{0,}\d{1,}$`
+	re := regexp.MustCompile(lessPatern)
+	less := re.FindAllString(input, 1)
+	if len(less) == 1 {
+		return "", fmt.Errorf("e2: %w", errorNotTwoOperands)
 	}
 
-	if !reflect.DeepEqual(expected, actual) {
-		fmt.Println(expected)
-		fmt.Println(actual)
-		t.Errorf("error, man")
-	}
-}
-
-func TestSumOfNegativeOperands(t *testing.T) {
-	s := "- 48 + -22"
-	expected := "-70"
-
-	actual, err := StringSum(s)
-
-	if err != nil {
-		t.Errorf(err.Error())
+	pattern := `^[\s\+-]{0,}[0-9a-z]{1,}[\s\+-]{0,}[0-9a-z]{1,}`
+	re = regexp.MustCompile(pattern)
+	remain := re.ReplaceAllString(input, "")
+	if len(remain) > 0 {
+		return "", fmt.Errorf("e3: %w", errorNotTwoOperands)
 	}
 
-	if !reflect.DeepEqual(expected, actual) {
-		fmt.Println(expected)
-		fmt.Println(actual)
-		t.Errorf("error, man")
+	var x, y int
+	var started, signMinus, definedX bool
+
+	for _, v := range input {
+		val := string(v)
+		if !definedX {
+			_, err := strconv.Atoi(val)
+			if err != nil {
+				if val == "-" {
+					signMinus = true
+				} else if val == "+" || val == " " {
+				} else {
+					return "", fmt.Errorf("e3: %w",
+						&strconv.NumError{
+							Func: "Atoi",
+							Num:  strconv.Itoa(x) + val,
+							Err:  strconv.ErrSyntax,
+						})
+				}
+				if started {
+					definedX = true
+				}
+			} else {
+				x, _ = strconv.Atoi(strconv.Itoa(x) + val)
+				if signMinus {
+					x = -x
+				} else {
+					x = x
+				}
+				signMinus = false
+				started = true
+			}
+		} else if definedX {
+			_, err := strconv.Atoi(val)
+			if err != nil {
+				if val == "-" {
+					signMinus = true
+				} else if val == "+" || val == " " {
+				} else {
+					return "", fmt.Errorf("e3: %w",
+						&strconv.NumError{
+							Func: "Atoi",
+							Num:  strconv.Itoa(y) + val,
+							Err:  strconv.ErrSyntax,
+						})
+				}
+			} else {
+				y, _ = strconv.Atoi(strconv.Itoa(y) + val)
+				if signMinus {
+					y = -y
+				} else {
+					y = y
+				}
+				signMinus = false
+
+			}
+		}
 	}
+
+	return strconv.Itoa(x + y), nil
 }
