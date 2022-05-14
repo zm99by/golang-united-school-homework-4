72f9719178b1b82b1cd8edf3c622d37d5bc8d77b
Vladimir Sakhonchik <Vladimir_Sakhonchik@epam.com>
2022-05-14 12:56:53 +0200

    add task 4

--- string_sum.go
+++ string_sum.go
@@ -2,26 +2,114 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"testing"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
-var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
-	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
-)
+func TestFailWhenEmptyInputStringSum(t *testing.T) {
+	s := "   "
+	expected := ""
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
+	actual, err := StringSum(s)
 
-func StringSum(input string) (output string, err error) {
-	return "", nil
+	if !reflect.DeepEqual(actual, expected) || err == nil || err.Error() != "e1: input is empty" {
+		fmt.Println(expected, len(expected))
+		fmt.Println(actual, len(actual))
+		t.Errorf("error, man")
+	}
+}
+
+func TestFailWhenLessTwoOperands(t *testing.T) {
+	s := "555"
+	expected := ""
+
+	actual, err := StringSum(s)
+
+	errorMessage := "e2: expecting two operands, but received more or less"
+	if !reflect.DeepEqual(actual, expected) || err == nil || errors.Unwrap(err).Error() == errorMessage {
+		fmt.Println(expected, len(expected))
+		fmt.Println(actual, len(actual))
+		t.Errorf(err.Error())
+	}
+}
+
+func TestFailWhenTooMuchOperands(t *testing.T) {
+	s := "-1 - 2 + 88"
+	expected := ""
+
+	actual, err := StringSum(s)
+
+	errorMessage := "e3: expecting two operands, but received more or less"
+	if !reflect.DeepEqual(actual, expected) || err == nil || errors.Unwrap(err).Error() == errorMessage {
+		fmt.Println(expected, len(expected))
+		fmt.Println(actual, len(actual))
+		t.Errorf(err.Error())
+	}
+}
+
+func TestFailWhenInputIncorrect(t *testing.T) {
+	s := "24c+55"
+	expected := ""
+	expectedErr := &strconv.NumError{Func: "Atoi", Num: "24c", Err: strconv.ErrSyntax}
+
+	actual, err := StringSum(s)
+
+	if !reflect.DeepEqual(actual, expected) || err == nil || !reflect.DeepEqual(errors.Unwrap(err), expectedErr) {
+		fmt.Println(expected, len(expected))
+		fmt.Println(actual, len(actual))
+		t.Errorf(err.Error())
+	}
+}
+
+func TestSumOfPositiveOperands(t *testing.T) {
+	s := "11 + 33"
+	expected := "44"
+
+	actual, err := StringSum(s)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		fmt.Println(expected)
+		fmt.Println(actual)
+		t.Errorf("error, man")
+	}
+}
+
+func TestSumOfDifferentOperands(t *testing.T) {
+	s := "-50 + 1"
+	expected := "-49"
+
+	actual, err := StringSum(s)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		fmt.Println(expected)
+		fmt.Println(actual)
+		t.Errorf("error, man")
+	}
+}
+
+func TestSumOfNegativeOperands(t *testing.T) {
+	s := "- 48 + -22"
+	expected := "-70"
+
+	actual, err := StringSum(s)
+
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	if !reflect.DeepEqual(expected, actual) {
+		fmt.Println(expected)
+		fmt.Println(actual)
+		t.Errorf("error, man")
+	}
 }
